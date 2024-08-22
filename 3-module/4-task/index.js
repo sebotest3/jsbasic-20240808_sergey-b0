7972d2f9af8c4d7f888c282df55cf48636bd931d
Sergey Bocharov <sergbocha91@yandex-team.ru>
2024-08-22 13:27:22 +0300

    решения задач 5-го модуля, авторские решения не смотрел(случайно кликнул)

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  return users
+    .filter(user => user.age <= age)
+    .map(user => `${user.name}, ${user.balance}`)
+    .join('\n');
 }
+
+console.log(showSalary(users, 40)); // выведет зарплаты пользователей до 40 лет
+console.log(showSalary(users, 30)); // выведет зарплаты пользователей до 30 лет
+// в учебнике случайно нажал на кнопку сравнить с решением автора, я его не смотрел, даже не обновлял гит, прошу не обнулять решение)
